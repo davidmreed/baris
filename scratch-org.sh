78e6ff929296e03d876a5a673c95d87030c1b190
David Reed <david@ktema.org>
2021-11-16 15:15:13 -0700

    Change package name

--- scratch-org.sh
+++ scratch-org.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/bash
 
-sfdx force:org:create -f sfdx/config/project-scratch-def.json -a sift
+sfdx force:org:create -f sfdx/config/project-scratch-def.json -a baris
 source refresh-token.sh
